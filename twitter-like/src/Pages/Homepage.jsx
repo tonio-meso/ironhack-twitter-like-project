10ff9c543d84a88680b5a818e9c0a97499ac2e9c
Tonio9802984 <antoine.prevors@gmail.com>
2023-05-11 16:31:01 +0200

    skeleton v2 and add link for you part

--- twitter-like/src/Pages/Homepage.jsx
+++ twitter-like/src/Pages/Homepage.jsx
@@ -1,11 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 function Homepage() {
   return (
-    <div>
-      <header> twitter project babieeees</header>
-      <h1>Hello there</h1>
-    </div>
+    <>
+      <div id="sidebar">
+        <h1>Like twitter but better</h1>
+        <div>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div id="search-spinner" aria-hidden hidden={true} />
+            <div className="sr-only" aria-live="polite"></div>
+          </form>
+          <form method="post">
+            <Link to="AvatarCreator">
+              <button type="submit">New Avatar</button>
+            </Link>
+          </form>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href={`/contacts/1`}>Your Profile 1</a>
+            </li>
+            <li>
+              <a href={`/contacts/2`}>Your Profil 2</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail"></div>
+    </>
   );
 }
 
