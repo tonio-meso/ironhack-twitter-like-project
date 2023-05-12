a4caf45875086a9ffbddede4715b77636e66c3dc
Toheeb Ashorobi <tasho646fr@gmail.com>
2023-05-12 12:39:06 +0200

    descirption structure in place

--- twitter-like/src/Pages/DescriptionPage.jsx
+++ twitter-like/src/Pages/DescriptionPage.jsx
@@ -1,11 +1,49 @@
 import React from "react";
-import { Link, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const floApi = "https://ironrest.fly.dev/api/avatar-collection";
 const apiUrl = "https://api.dicebear.com/6.x/pixel-art/svg?seed=";
+//I can implement new fields that the user can input to describe more sides of themselves.
 
 function DescriptionPage() {
-  return <div>DescriptionPage</div>;
+  const [singleUser, setSingleUser] = useState([]);
+  const params = useParams();
+
+  function getUserInfo() {
+    axios
+      .get(floApi)
+      .then((response) => {
+        console.log(response.data);
+        setSingleUser(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  const userId = singleUser.filter((user) => {
+    return beer._id === params._id;
+  })[0];
+
+  return (
+    <>
+      <div>Your Profile</div>
+      {userId && (
+        <>
+          <img src={userId.image} />
+          <h3>{userId.name}</h3>
+          <h3>{userId.lastName}</h3>
+          <h3>{userId.description}</h3>
+        </>
+      )}
+    </>
+  );
 }
 
 export default DescriptionPage;
