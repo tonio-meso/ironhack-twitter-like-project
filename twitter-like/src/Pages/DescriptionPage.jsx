73dd60c4c1b8b249571006c3051bbbbdd8a55847
Toheeb Ashorobi <tasho646fr@gmail.com>
2023-05-12 11:21:40 +0200

    here's your link

---
+++ twitter-like/src/Pages/DescriptionPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link, useState } from "react";
+
+const floApi = "https://ironrest.fly.dev/api/avatar-collection";
+const apiUrl = "https://api.dicebear.com/6.x/pixel-art/svg?seed=";
+
+function DescriptionPage() {
+  return <div>DescriptionPage</div>;
+}
+
+export default DescriptionPage;
