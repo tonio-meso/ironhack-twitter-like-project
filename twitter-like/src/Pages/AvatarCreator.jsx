9a0b326021fb01ef4cef8ec2bb85f0dcd3294ae9
Toheeb Ashorobi <tasho646fr@gmail.com>
2023-05-12 10:12:33 +0200

    stick to your file

--- twitter-like/src/Pages/AvatarCreator.jsx
+++ twitter-like/src/Pages/AvatarCreator.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { useState } from "react";
-import { Link } from "react";
+import { useState, Link } from "react";
 const apiUrl = "https://api.dicebear.com/6.x/pixel-art/svg?seed=";
+// const apiUrl = "https://api.dicebear.com/6.x/pixel-art/jpg?seed=";
+// const apiUrl = "https://api.dicebear.com/6.x/pixel-art/png?seed=";
 const floApi = "https://ironrest.fly.dev/api/avatar-collection";
+
 import axios from "axios";
 //I need the API to store users information.
 //How to determine which file endpoint will refer to when a user is inputting thier informaiton?
@@ -10,31 +12,34 @@ import axios from "axios";
 function AvatarCreator() {
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [image, setImage] = useState("");
+  //i might try to input an image directly and use this as a placeholder
   const [description, setDescription] = useState("");
 
   console.log(apiUrl + `${name}`);
 
   function handleSubmit(event) {
     event.preventDefault();
-    let image;
-
-    const userObject = {
-      name: name,
-      lastName: lastName,
-      image: image,
-      description: description,
-    };
+    // let image;
 
     axios
       .get(apiUrl + `${name}`)
       .then((response) => {
+        setImage(response.data);
         // console.log(response.data);
-        image = response.data;
-        console.log(image);
+        // image = response.data;
+        // console.log(image);
       })
       .catch((error) => {
         console.log(error);
       });
+
+    const userObject = {
+      name: name,
+      lastName: lastName,
+      image: `https://api.dicebear.com/6.x/open-peeps/svg?seed=${name}`,
+      description: description,
+    };
 
     axios
       .post(floApi, userObject)
@@ -61,17 +66,21 @@ function AvatarCreator() {
           value={name}
           onChange={(event) => setName(event.target.value)}
         />
-        {/* <img src="../../public/testPicture.svg" /> */}
+        {/* since im currently not able to figure out why i cant post the stored image to flo's api i had to figure out another way to get a random image. I think what I wrote below is a good start. Still thinking on how to make one of the randomized image belong to a specific user.  */}
         <label>Last Name:</label>
         <input
           type="text"
           value={lastName}
           onChange={(event) => setLastName(event.target.value)}
         />
+        <img
+          src={"https://api.dicebear.com/6.x/open-peeps/svg?seed=" + `${name}`}
+          alt="avatar"
+        />
 
         <label>Description:</label>
-        <input
-          type="text"
+        <textarea
+          type="textbox"
           value={description}
           onChange={(event) => setDescription(event.target.value)}
         />
