44d98487bc1343a7bbb4a0d71a0666735da67189
Tonio9802984 <antoine.prevors@gmail.com>
2023-05-16 18:02:19 +0200

    search bar activated

--- twitter-like/src/Components/DisplayAvatar.jsx
+++ twitter-like/src/Components/DisplayAvatar.jsx
@@ -5,77 +5,72 @@ import { Link } from "react-router-dom";
 
 const apiUrl = "https://ironrest.fly.dev/api/avatar-collection";
 
-const apiUrlDelete = "https://ironrest.fly.dev/api/avatar-collection";
+const DisplayAvatar = () => {
+  const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
-// Get your Avatar
-function GetAvatar() {
-  // i start to create a useState Hook to store the data
-  const [data, setData] = useState("");
+  useEffect(() => {
+    getAllData();
+  }, []);
 
   const getAllData = () => {
     axios
       .get(apiUrl)
       .then((response) => {
-        //console.log(response.data);
-        setData(response.data);
+        setData(response.data || []);
       })
       .catch((error) => {
-        //console.log(error);
+        console.log(error);
       });
   };
-  useEffect(() => {
-    getAllData();
-  }, []);
 
-  // Delete avatar and update after
-  function handleDelete(_id) {
-    axios
-      .delete(`${apiUrlDelete}/${_id}`)
-      .then(() => {
-        setData((prevData) => prevData.filter((item) => item._id !== _id));
-      })
-      .catch((error) => {
-        //console.log(error);
-      });
-  }
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredData = data.filter((item) => {
+    const { nickName, description } = item;
+    const lowerCaseQuery = searchQuery.toLowerCase();
+    return (
+      nickName.toLowerCase().includes(lowerCaseQuery) ||
+      description.toLowerCase().includes(lowerCaseQuery)
+    );
+  });
+
   return (
-    <>
-      <div id="avatar-container">
-        {data ? (
-          <ul id="avatar-list">
-            {data.map((item) => {
-              return (
-                <li key={item._id} className="avatar-item">
-                  <Link
-                    to={`/description-page/${item._id}`}
-                    className="avatar-link"
-                  >
-                    {item.image && (
-                      <div className="avatar">
-                        <img src={item.image} alt="avatar" />
-                      </div>
-                    )}
-                    <div className="nickname">{item.nickName}</div>
-                    {/* <span className="username">{item.name}</span> */}
-                    {/* <span className="lastname">{item.lastName}</span> */}
-                    <div className="description">{item.description}</div>
-                  </Link>
-                  {/* <button
-                    className="delete-button"
-                    onClick={() => handleDelete(item._id)}
-                  >
-                    Delete
-                  </button> */}
-                </li>
-              );
-            })}
-          </ul>
-        ) : (
-          <h3>No data yet</h3>
-        )}
+    <div id="avatar-container">
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={handleSearch}
+        />
       </div>
-    </>
+      {filteredData.length > 0 ? (
+        <ul id="avatar-list">
+          {filteredData.map((item) => (
+            <li key={item._id} className="avatar-item">
+              <Link
+                to={`/description-page/${item._id}`}
+                className="avatar-link"
+              >
+                {item.image && (
+                  <div className="avatar">
+                    <img src={item.image} alt="avatar" />
+                  </div>
+                )}
+                <div className="nickname">{item.nickName}</div>
+                <div className="description">{item.description}</div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <h3>No matching data</h3>
+      )}
+    </div>
   );
-}
+};
 
-export default GetAvatar;
+export default DisplayAvatar;
